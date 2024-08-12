5553bae2fd5264d9fd51105516217943feb5aac4
Ali ✨ <103325400+dino-ali@users.noreply.github.com>
2024-08-12 16:40:43 -0500

    Create ola.r

---
+++ ola.r
@@ -0,0 +1,38 @@
+# suma (se corre linea por linea)
+c<- 2**3
+c
+
+# multiplicación
+c<- 2*(5-8)+9/7
+
+vecto1<-c(1,2,13,4,5,7,8)
+vecto1
+
+#llenado de matriz por columnas
+matri1<-matrix(data = vecto1, nrow = 3, ncol = 2, byrow = FALSE)
+matri1
+
+#llenado de matriz por filas
+matri2<-matrix(data = vecto1, nrow = 3, ncol = 4, byrow = TRUE)
+matri2
+
+#producto escalar de matrices
+c1<-2*matri2
+c1
+
+c2<-2+matri2
+c2
+
+matri3<-matrix(data = vecto1, nrow = 3, ncol = 3, byrow = TRUE)
+matri3
+
+#tarea :busca la inversa producto y potencia de una matriz en r y hacer ejemplos de 3x3
+#como definir funciones en r, y hacer una funcion que sume los 10 primeros numeros
+#como hacr integrales en r
+
+mm<-matri3
+mmm<-mm*matri3
+mmm
+
+a1<-power(2)
+a1
